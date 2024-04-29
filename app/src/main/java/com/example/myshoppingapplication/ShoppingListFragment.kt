68bd0f0342ba5bdc98aaa559cf262bb116fa2a25
Mike Kemakolam <mikekemakolam@MBPM2-HGKM633XMW-KEMAKOLAM.local>
2024-04-29 19:41:12 -0400

    initial commit

---
+++ app/src/main/java/com/example/myshoppingapplication/ShoppingListFragment.kt
@@ -0,0 +1,76 @@
+package com.example.myshoppingapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SimpleAdapter
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ShoppingListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ShoppingListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_shopping_list, container, false)
+
+        val listView = view.findViewById<ListView>(R.id.list)
+        val courseList = mutableListOf("C-Programming", "Data Structure", "Database", "Python", "Java", "Operating System","Compiler Design", "Android Development");
+
+//        var arrayAdapter = SimpleAdapter(context, R.layout.fragment_shopping_list, courseList);
+
+
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ShoppingListFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ShoppingListFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
