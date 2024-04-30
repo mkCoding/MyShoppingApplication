589fc191a45c8ee10b6af63d663b064d124cf665
Mike Kemakolam <mikekemakolam@MBPM2-HGKM633XMW-KEMAKOLAM.local>
2024-04-30 18:30:47 -0400

    added UI updates to category fragment also implemented some navigation

--- app/src/main/java/com/example/myshoppingapplication/CategoryFragment.kt
+++ app/src/main/java/com/example/myshoppingapplication/CategoryFragment.kt
@@ -5,36 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.fragment.findNavController
+import com.example.myshoppingapplication.databinding.FragmentCategoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CategoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class CategoryFragment : Fragment() {
+
+    private var _binding:FragmentCategoryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+
+        val root:View = binding.root
+
+        val clothingCardView:CardView = binding.cvClothing
+        clothingCardView.setOnClickListener {
+            //move to next screen
+            findNavController().navigate(R.id.action_nav_category_to_clothingCategoryFragment)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        return root
     }
 
     companion object {
---
+++ app/src/main/java/com/example/myshoppingapplication/ui/ClothingCategoryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.myshoppingapplication.ui
+
+import androidx.lifecycle.ViewModel
+//Extending functionalities from Model
+class ClothingCategoryViewModel : ViewModel(){
+
+
+}
