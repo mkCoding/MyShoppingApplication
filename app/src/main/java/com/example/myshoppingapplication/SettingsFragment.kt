61f7ff9be5ed4851f441046813abb36aaa3c8fd9
mkCoding <mkem50@gmail.com>
2024-04-30 00:54:48 -0400

    Added toast messages to settings options

--- app/src/main/java/com/example/myshoppingapplication/SettingsFragment.kt
+++ app/src/main/java/com/example/myshoppingapplication/SettingsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +35,44 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view =  inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val profile_settings = view.findViewById<CardView>(R.id.cvProfileSettings)
+        val notification = view.findViewById<CardView>(R.id.cvNotifications)
+        val appearance = view.findViewById<CardView>(R.id.cvAppearance)
+        val security = view.findViewById<CardView>(R.id.cvPrivacySecurity)
+        val help = view.findViewById<CardView>(R.id.cvHelp)
+        val about = view.findViewById<CardView>(R.id.cvAbout)
+
+        profile_settings.setOnClickListener {
+            Toast.makeText(context,"Profile Settings",Toast.LENGTH_LONG).show()
+        }
+
+        notification.setOnClickListener {
+            Toast.makeText(context,"Notifications",Toast.LENGTH_LONG).show()
+        }
+
+        appearance.setOnClickListener {
+            Toast.makeText(context,"Appearance Clicked",Toast.LENGTH_LONG).show()
+        }
+        security.setOnClickListener {
+            Toast.makeText(context,"Security Clicked!",Toast.LENGTH_LONG).show()
+        }
+
+        help.setOnClickListener {
+            Toast.makeText(context,"Help clicked",Toast.LENGTH_LONG).show()
+        }
+
+        about.setOnClickListener {
+            Toast.makeText(context,"About Clicked!",Toast.LENGTH_LONG).show()
+        }
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return view
     }
 
     companion object {
