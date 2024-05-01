c1a3144f3a236d569c319407c445362759b7e048
mkCoding <mkem50@gmail.com>
2024-05-01 00:21:59 -0400

    Adding code for clothing category search and display functionality via ViewModel

--- app/src/main/java/com/example/myshoppingapplication/ClothingCategoryFragment.kt
+++ app/src/main/java/com/example/myshoppingapplication/ClothingCategoryFragment.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import com.example.myshoppingapplication.databinding.FragmentCategoryBinding
+import com.example.myshoppingapplication.databinding.FragmentClothingCategoryBinding
+import com.example.myshoppingapplication.ui.ClothingCategoryViewModel
 
 
 /**
@@ -14,12 +22,63 @@ import android.view.ViewGroup
  */
 class ClothingCategoryFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private lateinit var binding: FragmentClothingCategoryBinding
+    private lateinit var adapter:ArrayAdapter<String>
+    lateinit var viewModel:ClothingCategoryViewModel
+
+//    private val dataList = listOf(
+//        "Blue Striped Button-Up Shirt", "White Graphic T-shirt", "Black V-neck Sweater", "Pink Crop Top",
+//        "Black Leggings","Polka Dot Wrap Dress", "Leather Jacket","Yoga Pants",
+//        "Floral Maxi Dress", "Trench Coat", "Bikini", "One-Piece Swimsuit", "Swim Cover-Up")
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        //initialize bindinv variable so that layoutview references are easier to access
+        binding = FragmentClothingCategoryBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clothing_category, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[ClothingCategoryViewModel::class.java]
+
+        //initialize the array adapter
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1)
+
+        //link the list view with the adapter so it understands the list that we are tying to the code
+        binding.lvClothingProducts.adapter = adapter
+
+        //this will observe the origin list pulled from ClothingCategoryViewModel
+        viewModel.dataList.observe(viewLifecycleOwner) { data ->
+            adapter.clear()
+            adapter.addAll(data) //add data from origin list to adapter (this is obtained from view model)
+
+        }
+
+        //this will Observe the filtered clothing list from ClothingCategoryViewModel
+        viewModel.filteredDataList.observe(viewLifecycleOwner) { filteredData ->
+            // Update the adapter with the filtered data
+            adapter.clear()
+            adapter.addAll(filteredData) //add data from filtered list to adapter
+        }
+
+        //this will handle the textListener and query text change
+        binding.svSearchAllProducts.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            //when the text is changed update/filter the list
+            override fun onQueryTextChange(newText: String?): Boolean {
+//                adapter.filter.filter(newText.orEmpty())
+                viewModel.filterData(newText) //when this method is hit use viewModel to filter data
+                return true
+            }
+        })
+
     }
 
 
--- app/src/main/java/com/example/myshoppingapplication/ui/ClothingCategoryViewModel.kt
+++ app/src/main/java/com/example/myshoppingapplication/ui/ClothingCategoryViewModel.kt
@@ -1,8 +1,39 @@
 package com.example.myshoppingapplication.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 //Extending functionalities from Model
+
+
+//the view model holds live data that will be observed in my fragment (ClothingCategoryFragment)
 class ClothingCategoryViewModel : ViewModel(){
 
+    private val _dataList = MutableLiveData<List<String>>()
+    val dataList: LiveData<List<String>> = _dataList
+
+    // Filtered list
+    private val _filteredDataList = MutableLiveData<List<String>>()
+    val filteredDataList: LiveData<List<String>> = _filteredDataList //create filtered list that will hold filtered data as user adds input
+
+    init {
+        // Simulate data loading
+        loadDummyData()
+    }
+
+    private fun loadDummyData() {
+        val dummyData = listOf( "Blue Striped Button-Up Shirt", "White Graphic T-shirt", "Black V-neck Sweater", "Pink Crop Top",
+            "Black Leggings","Polka Dot Wrap Dress", "Leather Jacket","Yoga Pants",
+            "Floral Maxi Dress", "Trench Coat", "Bikini", "One-Piece Swimsuit", "Swim Cover-Up") // Your data list
+        _dataList.value = dummyData
+
+    }
+
+    fun filterData(newText: String?){
+        val originalList = _dataList.value ?: emptyList()
+        //only return elements when query is not null or if its blank
+        //also added functionality so that user can enter in any case letters and results still show up
+        _filteredDataList.value = originalList.filter { it.toLowerCase().contains(newText.orEmpty().toLowerCase()) }
+    }
 
 }
